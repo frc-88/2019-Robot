69d338dfa555e559bd0bf55e3b2db0a646ea031b
PaulTerrasi <paul.a.terrasi@gmail.com>
2019-01-13 20:12:15 -0500

    Add Junit Testing

---
+++ src/test/java/frc/robot/PlaceholderTest.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class PlaceholderTest {
+
+    @Test
+    public void passingTest() {
+        assertTrue(true);
+    }
+}
