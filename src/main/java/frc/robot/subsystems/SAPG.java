4ecce25b10480a1c8169c1d05f58a5140ef9a349
Mr. Nickm <navtges@gmail.com>
2019-02-17 08:54:21 -0500

    Add testing routine to SAPG

--- src/main/java/frc/robot/subsystems/SAPG.java
+++ src/main/java/frc/robot/subsystems/SAPG.java
@@ -5,37 +5,38 @@ import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
 import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 
 public class SAPG extends Subsystem{
-    
-
     private static final double Kp = 0;
     private static final double Ki = 0;
     private static final double Kd = 0;
-    private static final double leftLimit = 0;
-    private static final double rightLimit = 0;
+    private static final double forwardLimit = 0;
+    private static final double reverseLimit = 0;
     private static final double center = 0;
 
-    private TalonSRX sideMotor;
+    private WPI_TalonSRX sideMotor;
     private DoubleSolenoid deployPiston;
     private DoubleSolenoid grabPiston;
     private PIDController sapgController;
 
     public SAPG(){
-        sideMotor = new TalonSRX(RobotMap.SAPG_MOTOR_ID);
+        sideMotor = new WPI_TalonSRX(RobotMap.SAPG_MOTOR_ID);
         deployPiston = new DoubleSolenoid(RobotMap.SAPG_DEPLOY_PCM, RobotMap.SAPG_DEPLOY_FORWARD, RobotMap.SAPG_DEPLOY_REVERSE);
         grabPiston = new DoubleSolenoid(RobotMap.SAPG_GRAB_PCM, RobotMap.SAPG_GRAB_FORWARD, RobotMap.SAPG_GRAB_REVERSE);
 
-        //sapgController = new PIDController(Kp, Ki, Kd, Robot.m_limelight_back, sideMotor);
-        //sapgController.disable();
+        sapgController = new PIDController(Kp, Ki, Kd, Robot.m_limelight_back, sideMotor);
+        sapgController.disable();
+
+        initPreferences();
     }
 
     public void configureTalon(){
@@ -43,10 +44,6 @@ public class SAPG extends Subsystem{
         sideMotor.setNeutralMode(NeutralMode.Coast);
         sideMotor.setInverted(false);
         sideMotor.configFeedbackNotContinuous(false, RobotMap.CAN_TIMEOUT);
-        sideMotor.configForwardLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen);
-        sideMotor.configReverseLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen);
-        sideMotor.configForwardSoftLimitEnable(true);
-        sideMotor.configReverseSoftLimitEnable(true);
         sideMotor.configSelectedFeedbackSensor(FeedbackDevice.Analog);
         sideMotor.configMotionCruiseVelocity(5);
         sideMotor.configMotionAcceleration(5);
@@ -54,21 +51,38 @@ public class SAPG extends Subsystem{
         sideMotor.config_kI(0, 0);
         sideMotor.config_kD(0, 0);
         sideMotor.config_kF(0 , 0);
+
+        // sideMotor.configForwardSoftLimitThreshold(forwardLimit)
+        // sideMotor.configReverseSoftLimitThreshold(reverseLimit)
+        // sideMotor.configForwardSoftLimitEnable(true);
+        // sideMotor.configReverseSoftLimitEnable(true);
     }
 
-    public void openTheJaws(){
-        grabPiston.set(Value.kForward);
+    private void initPreferences() {
+        Preferences prefs = Preferences.getInstance();
+    
+        if (!prefs.containsKey("SAPGTestOutput")) { prefs.putDouble("SAPGTestOutput", 0.0); }
     }
 
     public void shiftTheSAPG(double position){
-        sideMotor.set(ControlMode.Position, position);
+        sideMotor.set(ControlMode.MotionMagic, position);
     }
     
     public void set(double percentOutput){
         sideMotor.set(ControlMode.PercentOutput, percentOutput);
     }
 
-    
+    public boolean atForwardLimit() {
+        return sideMotor.getSelectedSensorPosition() > (forwardLimit - 30);
+    }
+
+    public boolean atReverseLimit() {
+        return sideMotor.getSelectedSensorPosition() < (reverseLimit + 30);
+    }
+
+    public void openTheJaws(){
+        grabPiston.set(Value.kForward);
+    }
 
     public void forwardPush(){
         deployPiston.set(Value.kForward);
