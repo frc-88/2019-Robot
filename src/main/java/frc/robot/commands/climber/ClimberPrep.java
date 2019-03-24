9db0ad8f71463fe2019c12ad40bf7ef2c6d614a4
Nick Avtges <navtges@gmail.com>
2019-03-24 15:55:20 -0400

    notes about one climber test

--- src/main/java/frc/robot/commands/climber/ClimberPrep.java
+++ src/main/java/frc/robot/commands/climber/ClimberPrep.java
@@ -15,7 +15,7 @@ public class ClimberPrep extends Command {
 
   private Climber climber = Robot.m_climber;
 
-  private final int WINCH_DISTANCE = 100000;
+  private final int WINCH_DISTANCE = 100000; //54,000 string wound wrong way
 
   public ClimberPrep() {
     requires(climber);
