4467d633f2a44960cbb9258c8ab490ef59288f71
paulterrasi <navtges@gmail.com>
2019-04-11 13:17:49 -0400

    Q1

--- src/main/java/frc/robot/commands/climber/ClimberBasicControl.java
+++ src/main/java/frc/robot/commands/climber/ClimberBasicControl.java
@@ -23,7 +23,7 @@ public class ClimberBasicControl extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.m_climber.setVoltage(Robot.m_oi.getOperatorRightYAxis());
+    Robot.m_climber.setVoltage(Math.pow(Robot.m_oi.getOperatorRightYAxis(),3));
   }
 
   // Make this return true when this Command no longer needs to run execute()
